08a4bfa3b6bf5f0ac476ce1bf33a9848404cdbc4
Brandon Semilla <grandonbroseph@gmail.com>
2016-10-15 22:17:43 -0700

    Add scroll gimmick

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -5,12 +5,12 @@ new Vue({
   el: '#wrap'
 })
 
-var item = document.querySelector('#screen')
+var item = document.querySelector('#screen');
 var header = document.querySelector('#header');
 var scrollPos, scrollMax, scrollTick;
 window.addEventListener("scroll", function(e) {
   scrollPos = document.body.scrollTop;
-  scrollMax = item.getBoundingClientRect().height - header.getBoundingClientRect().height;
+  scrollMax = item.getBoundingClientRect().height - header.getBoundingClientRect().height - 1;
   if (!scrollTick) {
     window.requestAnimationFrame(function() {
       if (scrollPos > scrollMax) {
@@ -23,6 +23,33 @@ window.addEventListener("scroll", function(e) {
   }
   scrollTick = true;
 });
+
+document.querySelector('#screen-arrow').addEventListener('click', function() {
+  var velocity = .5
+  var speed = 1.08;
+  var bounce = .2
+  var finito = false
+  var bouncing = false
+  var max = item.getBoundingClientRect().height - header.getBoundingClientRect().height;
+  function loop() {
+    if (!bouncing)
+      velocity *= speed
+    else
+      velocity += speed
+    document.body.scrollTop += velocity
+    if (document.body.scrollTop > max) {
+      document.body.scrollTop = max
+      velocity *= -bounce
+      bouncing = true
+      setTimeout(function() {
+        finito = true
+      }, 1000)
+    }
+    if (!finito)
+      window.requestAnimationFrame(loop)
+  }
+  loop()
+})
 
 },{"./parelax":2}],2:[function(require,module,exports){
 module.exports = (function() {
